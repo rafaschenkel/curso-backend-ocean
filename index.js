299adc44b0e320400c053c0f524eab942498f641
Rafael Schenkel <rafaschenkel.dev@gmail.com>
2023-08-28 12:07:09 -0300

    Criaçao do endpoint `/oi`

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+app.get('/oi', function (req, res) {
+    res.send('Olá Mundo');
+});
+
 app.listen(3000, () => {
     console.log('Servidor rodando http://localhost:3000');
 });
